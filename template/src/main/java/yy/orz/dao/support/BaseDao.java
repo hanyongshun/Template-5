36f8988d6f3660171deeb6415dcb1430db9aae46
p54088 <p54088@163.com>
2014-08-22 14:29:53 +0800

    add common

--- template/src/main/java/yy/orz/dao/support/BaseDao.java
+++ template/src/main/java/yy/orz/dao/support/BaseDao.java
@@ -3,9 +3,7 @@ package yy.orz.dao.support;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.io.Serializable;
-
-public interface BaseDao<T, PK extends Serializable> extends PagingAndSortingRepository<T, PK>, JpaSpecificationExecutor<T> {
+public interface BaseDao<T> extends PagingAndSortingRepository<T, Long>, JpaSpecificationExecutor<T> {
 
 	
 }
